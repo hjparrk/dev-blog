3cfccfbfb0a288fdaafce4366410c760abc1827b
hjparrk <hyojoon2372552@naver.com>
2022-08-30 23:15:00 +0900

    update

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,23 +1,18 @@
 const axios = require("axios");
 
 export async function getAllAboutItems() {
-  const response = await axios.get(
-    "https://dev-blog-theta-five.vercel.app/api/home/about-item"
-  );
+  const response = await axios.get("/api/home/about-item");
   const data = response.data;
   const aboutItems = data.abouts;
   return aboutItems;
 }
 
 export async function deleteAboutItemByTitle(title) {
-  const response = await axios.delete(
-    "https://dev-blog-theta-five.vercel.app/api/home/about-item",
-    {
-      data: {
-        title: title,
-      },
-    }
-  );
+  const response = await axios.delete("/api/home/about-item", {
+    data: {
+      title: title,
+    },
+  });
 
   return;
 }
