caa60395d75af8b8a3c4c3d08bbd412c01306c47
hjparrk <hyojoon2372552@naver.com>
2022-08-30 23:23:34 +0900

    update

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,18 +1,23 @@
 const axios = require("axios");
 
 export async function getAllAboutItems() {
-  const response = await axios.get("/api/home/about-item");
+  const response = await axios.get(
+    "https://blog-self-beta.vercel.app/api/home/about-item"
+  );
   const data = response.data;
   const aboutItems = data.abouts;
   return aboutItems;
 }
 
 export async function deleteAboutItemByTitle(title) {
-  const response = await axios.delete("/api/home/about-item", {
-    data: {
-      title: title,
-    },
-  });
+  const response = await axios.delete(
+    "https://blog-self-beta.vercel.app//api/home/about-item",
+    {
+      data: {
+        title: title,
+      },
+    }
+  );
 
   return;
 }
