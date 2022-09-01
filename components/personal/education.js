dd454aa618946b6772a9ade6970b96c427d48d49
hjparrk <hyojoon2372552@naver.com>
2022-09-01 14:47:12 +0900

    fix some typos

--- components/personal/education.js
+++ components/personal/education.js
@@ -3,7 +3,7 @@ const Education = (props) => {
     <div className="flex flex-col my-10 items-start justify-start max-w-xl mr-14 pr-2">
       <div className="my-5">
         <h1 className="text-4xl font-extrabold my-10">Education</h1>
-        <h3 className="text-xl font-light">2018-07 - 2020.06</h3>
+        <h3 className="text-xl font-light">2018-07 - 2020-06</h3>
         <h2 className="text-2xl font-semibold mb-1">
           the University of Sydney, Australia
         </h2>
@@ -15,14 +15,14 @@ const Education = (props) => {
         </h3>
       </div>
       <div className="my-5">
-        <h3 className="text-xl font-light">2020-07 - 2020.12</h3>
+        <h3 className="text-xl font-light">2020-07 - 2020-12</h3>
         <h2 className="text-2xl font-semibold mb-1">
           Leave of Absence (the University of Sydney)
         </h2>
         <h3 className="text-xl font-normal mb-5">COVID-19 Pandemic</h3>
       </div>
       <div className="my-5">
-        <h3 className="text-xl font-light">2021-03 - 2022.12</h3>
+        <h3 className="text-xl font-light">2021-03 - 2022-12</h3>
         <h2 className="text-2xl font-semibold mb-1">
           Leave of Absence (the University of Sydney)
         </h2>
