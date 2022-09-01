f1111fb34cd9ea2029aedfb5486437195bdbc66d
hjparrk <hyojoon2372552@naver.com>
2022-09-01 14:58:18 +0900

    add dummy blog posts

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,34 @@
+const DUMMY_POSTS = [
+  {
+    title: "BLOG",
+    subtitle: "THIS IS MY BLOG POST",
+    description: "BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH ",
+  },
+  {
+    title: "BLOG",
+    subtitle: "THIS IS MY BLOG POST",
+    description: "BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH ",
+  },
+  {
+    title: "BLOG",
+    subtitle: "THIS IS MY BLOG POST",
+    description: "BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH ",
+  },
+];
+
 const Blog = () => {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-5xl">Blog</h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-5xl mb-10">Blog Pages</h1>
+      <ul className="flex flex-col justify-center items-center">
+        {DUMMY_POSTS.map((post) => (
+          <li key={post.title} className="mb-20 p-10">
+            <h1 className="text-5xl font-extrabold m-5">{post.title}</h1>
+            <h1 className="text-3xl font-medium m-5">{post.subtitle}</h1>
+            <h1 className="text-xl font-light m-5">{post.description}</h1>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
