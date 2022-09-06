0cfefcacc586eb4e96479ee53d5e300203b14e5c
hjparrk <hyojoon2372552@naver.com>
2022-09-06 17:16:31 +0900

    fix some responsive layout

--- components/home/welcoming.js
+++ components/home/welcoming.js
@@ -6,7 +6,7 @@ function Welcoming(props) {
       <h1 className="mt-40 mb-12 font-bold text-5xl">
         Wanna &nbsp;explore &nbsp;my &nbsp;blog?
       </h1>
-      <div className="flex flex-row mb-30 pb-40 text-4xl">
+      <div className="flex flex-row mb-40 pb-40 text-4xl">
         <h2 className="font-medium">highfive&nbsp; me&emsp;</h2>
         <motion.h2
           className="font-medium"
